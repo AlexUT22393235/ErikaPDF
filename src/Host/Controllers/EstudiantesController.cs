c4bb577df7c5053524b932974fa965f9e1ffe575
MaximoQuin <169080723+MaximoQuin@users.noreply.github.com>
2024-10-14 09:45:11 -0600

    Subiendo todo

---
+++ src/Host/Controllers/EstudiantesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ApplicationCore.Interfaces;
+
+namespace Host.Controllers
+{
+    [Route("api/estudiantes")]
+    [ApiController]
+    public class EstudiantesController : ControllerBase
+    {
+        private readonly IEstudiantesService _service;
+
+        public EstudiantesController(IEstudiantesService service)
+        {
+            _service = service;
+        }
+
+        // <sumary>
+        // Lisra de usuarios get
+        // </sumary>
+
+        [Route("get-estudiantes")]
+        [HttpGet]
+
+        public async Task<IActionResult> GetEstudiantes()
+        {
+            var result = await _service.GetEstudiantes();
+            return Ok(result);
+        }
+    }
+}
---
+++ src/Infraestructure/Services/EstudiantesService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationCore.Interfaces;
+using ApplicationCore.Wrappers;
+using AutoMapper;
+using Infraestructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infraestructure.Services
+{
+    public class EstudiantesService : IEstudiantesService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public EstudiantesService(ApplicationDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<Response<object>> GetEstudiantes(){
+            object list = new object();
+            list = await _dbContext.Estudiantes.ToListAsync();
+            return new Response<object>(list);
+        }
+    }
+
+        //Task<Response<object>> IEstudiantesService.GetEstudiantes()
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
